6f6e387def0842183ced03aeb4deed515b9ee357
Walker Leite <walker@incuca.com.br>
2018-01-02 12:05:52 -0200

    feat(tests): add validation tests

--- test/specs/ic-firebase-uploader.spec.js
+++ test/specs/ic-firebase-uploader.spec.js
@@ -1,17 +1,60 @@
-import Vue from 'vue';
-import Component from '../../src/ic-firebase-uploader/ic-firebase-uploader';
+import Vue from 'vue'
+import Component from '../../src/ic-firebase-uploader/ic-firebase-uploader'
 
 describe('ic-firebase-uploader.vue', () => {
-  const Constructor = Vue.extend(Component);
-  const propsData = {text: 'Test text'};
+  const Constructor = Vue.extend(Component)
+  let propsData
+
+  beforeEach(() => {
+    propsData = {}
+  })
 
   it('should instance the right component', () => {
-    const vm = new Constructor({propsData});
-    expect(vm.$options.name).to.equal('ic-firebase-uploader');
-  });
+    const vm = new Constructor({propsData})
+    expect(vm.$options.name).to.equal('ic-firebase-uploader')
+  })
 
-  it('should render correct content', () => {
-    const vm = new Constructor({propsData}).$mount();
-    expect(vm.$el.innerHTML).to.equal(propsData.text);
-  });
+  it.skip('max-files prop should be required', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('path prop should be required', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('getFileName prop should be required', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('getFileName should be called with a file', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('max-files should limit the number of uploaded files', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('files should match path of firebase', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('should emit upload on file upload with fullPath and doUpload', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('calling doUpload should upload the file', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('should emit delete on file deletion with fullPath and doDelete', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('calling doDelete should delete the file', () => {
+    // TODO: Implement test
+  })
+
+  it.skip('should emit click on clicking file', () => {
+    // TODO: Implement test
+  })
 });
